e23a62631e57aaf748b4607c69077b53a9b658d8
Mark Chaisson <mchaisso@usc.edu>
2018-03-13 15:41:51 -0700

    Adding binary quiver to avoid needing to install it.

---
+++ quiver_bin/setup-env.sh
@@ -0,0 +1,4 @@
+export   LD_LIBRARY_PATH=/usr/usc/gnu/gcc/5.3.0/lib64/:/home/cmb-16/mjc/mchaisso/projects/phasedsv/quiver/lib:$LD_LIBRARY_PATH
+export PATH=/home/cmb-16/mjc/mchaisso/projects/phasedsv/quiver/bin:$PATH
+source /usr/usc/gnu/gcc/5.3.0/setup.sh 
+
