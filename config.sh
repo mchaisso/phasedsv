3be1c1ca944bb944109a2ee64c1ef883b27e4de3
Mark Chaisson <mchaisso@usc.edu>
2018-04-23 13:03:20 -0700

    Added config.sh

---
+++ config.sh
@@ -0,0 +1,12 @@
+
+#
+# Add any custom modifications to nonstandard software locations
+#
+
+source /home/cmb-16/mjc/shared/virtualenvs/python2.7/bin/activate
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/usc/gnu/gcc/5.3.0/lib64
+
+export PATH=$cmb16/shared/bin:$PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:
+export PATH=$HOME/software/bin:$PATH
+
