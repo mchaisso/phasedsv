e38d04600d5da6075948041628f3d74bc004a196
Mark Chaisson <mchaisso@gmail.com>
2019-04-14 11:48:32 -0700

    Removing quiver bin

--- quiver_bin/lib/cmake/openblas/OpenBLASConfig.cmake
+++
@@ -1,3 +0,0 @@
-SET(OpenBLAS_VERSION "0.2.20")
-SET(OpenBLAS_INCLUDE_DIRS /panfs/cmb-panasas2/mchaisso/pitchfork/staging/OpenBLAS-0.2.20/include)
-SET(OpenBLAS_LIBRARIES /panfs/cmb-panasas2/mchaisso/pitchfork/staging/OpenBLAS-0.2.20/lib/libopenblas.so)
